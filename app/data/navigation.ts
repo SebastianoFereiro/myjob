f480ac63c026ee6681d0e881b8a4895669b06193
Seba <seba@irr.by>
2026-06-25 22:21:56 +0300

    [web] menu blog

--- app/data/navigation.ts
+++ app/data/navigation.ts
@@ -8,5 +8,6 @@ export const navigationItems: NavigationItem[] = [
   { name: "Вакансии", slug: "/jobs" },
  
   { name: "Компании", slug: "/companies" },
+   { name: "Статьи", slug: "/blog" },
   { name: "Контакты", slug: "/contacts" },
 ];
