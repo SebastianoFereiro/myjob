c2d395b6c08d7c4566caece7b2ff609918033172
Seba <seba@irr.by>
2026-06-29 22:35:27 +0300

    [web] section stack css mobile fix

--- components/tools-stack-section.tsx
+++ components/tools-stack-section.tsx
@@ -13,8 +13,8 @@ export async function ToolsStackSection() {
   if (top.length === 0) return null;
 
   return (
-    <section id="vacancies" className="w-full py-8">
-      <div className="mx-auto max-w-6xl space-y-10 px-4">
+    <section id="vacancies" className="w-full md:py-8 py-4">
+      <div className="mx-auto max-w-6xl space-y-10 ">
         <h2 className="mb-6 pb-6 text-4xl font-medium tracking-tight text-pretty text-foreground md:text-5xl lg:text-6xl">
           Вакансии по направлениям
         </h2>
@@ -25,7 +25,7 @@ export async function ToolsStackSection() {
               <Link href={`/jobs?category=${category.slug}#vacancies`} className="block">
                 <Card className="bg-hatch rounded-2xl border-0 p-2 shadow-none transition-colors hover:bg-accent/50">
                   <div className="flex items-center justify-between gap-2 md:gap-10">
-                    <div className="flex items-center gap-4">
+                    <div className="flex items-center gap-0 md:gap-4">
                       <div className="flex size-12 items-center justify-center rounded-2xl bg-background/70 text-lg font-bold text-foreground">
                         {category.name.charAt(0).toUpperCase()}
                       </div>
@@ -33,7 +33,7 @@ export async function ToolsStackSection() {
                       <div>
                         <h3 className="text-xl font-semibold tracking-tight">{category.name}</h3>
                         {category.description ? (
-                          <p className="text-xs uppercase text-foreground/50 md:text-sm">
+                          <p className="text-[8px] sm:text-xs uppercase text-foreground/50 md:text-sm">
                             {category.description}
                           </p>
                         ) : null}
@@ -42,7 +42,7 @@ export async function ToolsStackSection() {
 
                     <div className="pr-2 text-right font-semibold uppercase md:pr-5">
                       <span className="block text-2xl leading-none">{category.count ?? 0}</span>
-                      <span className="text-xs text-foreground/50">вакансий</span>
+                      <span className="text-[8px] sm:text-xs text-foreground/50">вакансий</span>
                     </div>
                   </div>
                 </Card>
